dab390c18d6f6069c51c0cb5d656ceee2dc7cbe5
Gerald Urschitz <gerald.urschitz@gmail.com>
2018-05-20 19:21:19 +0200

    fix issue with export of FirebaseConfigType

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 exports.FirebaseAuthProvider = require("./lib/index").FirebaseAuthProvider;
 exports.FirebaseAuthConsumer = require("./lib/index").FirebaseAuthConsumer;
+exports.FirebaseConfigType = require("./lib/index").FirebaseConfigType;

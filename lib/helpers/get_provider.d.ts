11cf39700d8a88f022ab573e0d4a8944cd424a6a
Gerald Urschitz <gerald.urschitz@gmail.com>
2018-05-21 11:06:29 +0200

    rebuild and bump version to 0.0.3

--- lib/helpers/get_provider.d.ts
+++ lib/helpers/get_provider.d.ts
@@ -1,3 +1,3 @@
 import * as firebase from 'firebase';
-declare const getProvider: (providerId: string) => firebase.auth.TwitterAuthProvider | firebase.auth.FacebookAuthProvider | firebase.auth.GoogleAuthProvider | firebase.auth.GithubAuthProvider | firebase.auth.EmailAuthProvider;
+declare const getProvider: (providerId: string) => firebase.auth.EmailAuthProvider | firebase.auth.FacebookAuthProvider | firebase.auth.GithubAuthProvider | firebase.auth.GoogleAuthProvider | firebase.auth.TwitterAuthProvider;
 export default getProvider;
